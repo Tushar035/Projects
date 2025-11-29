4b6d7d73c308f4aaab017ed25c92c115e90f4e5a
tushar patil <patilt035@gmail.com>
2025-11-29 11:45:48 +0530

    First commit React

---
+++ ReactFirstApp/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div> 
+        <h1>{count}</h1> 
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Increment
+        </button>
+        <button onClick={() => setCount(0)}>
+          Reset
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
